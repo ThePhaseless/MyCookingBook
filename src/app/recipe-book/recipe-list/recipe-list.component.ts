f5e3ee12016e0900fe0bbe458c288ebd8d16e807
ThePhaseless <33990351+ThePhaseless@users.noreply.github.com>
2023-02-22 23:59:25 +0100

    Recepie book init

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: RecipeItemComponent[] = [];
 }
